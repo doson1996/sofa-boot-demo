48b8b6177cc53b73cafe9f65f3963ae7acbda77f
ds <1159629515@qq.com>
2025-03-26 21:46:08 +0800

    init

---
+++ sofa-boot-provider/src/main/java/com/ds/sofa/boot/provider/Application.java
@@ -0,0 +1,13 @@
+package com.ds.sofa.boot.provider;
+
+/**
+ * @author ds
+ * @date 2025/3/26
+ * @description
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
